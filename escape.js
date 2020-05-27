e667e3ce62cb4f0808d4e7894f6763b2fe925c46
bigfish12 <2023felixs@psi.kiev.ua>
2020-05-27 16:42:23 +0300

    First draft

--- escape.js
+++ escape.js
@@ -7,7 +7,7 @@ clock.style.display = "none";
 const task1 = function () {
     const box = prompt("Voer het password hier in");
     const parsed = box.toUpperCase();
-    if (parsed == "FIBONACCI") {
+    if (parsed == "MONA LISA") {
         const alert1 = alert("Correct! Klick voor de volgende opdracht")
     } else return prompt("Incorrect. Probeer opnieuw")
 
@@ -18,6 +18,22 @@ const task1 = function () {
 
 }
 
-const button = document.querySelector(".btn1");
+const button = document.querySelector("#button-2");
 button.addEventListener("click", task1);
 
+const task2 = function () {
+    const box = prompt("Voer het password hier in");
+    const parsed = box.toUpperCase();
+    if (parsed == "FIBONACCI") {
+        const alert1 = alert("Correct! Klick voor de volgende opdracht")
+    } else return prompt("Incorrect. Probeer opnieuw")
+
+    console.log(window.alert1);
+    // if (window.alert1 === undefined) {
+    //     clock.style.display = "block";
+    // }
+
+}
+
+const button2 = document.querySelector("#button-3");
+button2.addEventListener("click", task2)
